ccd4d341e6fc1b5427e7d3e1c132b4b21b46d756
SRNelson1995 <oscarnelson15@yahoo.com>
2018-06-08 09:43:33 -0600

    1

---
+++ SystemBec/app/Http/Controllers/TallerController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TallerController extends Controller
+{
+    /*Sirve para la creacion de actividades pantalla donde se crea con todos los campos*/
+    public function create(){
+    return view('templates.admin.taller.agregar');
+						                }
+	 public function mostrar(){
+    return view('templates.admin.taller.index');
+						                }
+}
